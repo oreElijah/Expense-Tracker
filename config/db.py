0e6437b45ea229b424956f87a1da43ecb602ad89
Elijah Ore <oreelijah33@gmail.com>
2025-01-03 00:11:08 +0100

    Updating database

--- config/db.py
+++ config/db.py
@@ -8,17 +8,3 @@ db = client["expense-tracker"]
 
 dbs = db["users"]
 expense_db = db["expenses"]
-
-data = {
-    "UserName": "admin",
-    "Email": "ooooo",
-    "Password": "admin"
-}
-
-expense_data = {
-    "category": "Food",
-    "Amount": 300,
-    "Date": "2021-08-12"
-}
-
-dbs.insert_one(data)
